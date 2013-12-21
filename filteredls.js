46b7bf3c00f57474fd995f67fe34693d2de24d39
Colin Mackey <colin@fishbrains.com>
2013-12-21 15:41:04 -0500

    new lessons

--- filteredls.js
+++ filteredls.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var patt = new RegExp('\\.' + process.argv[3] + '$');
 fs.readdir(process.argv[2], function(err, list) {
+	if (err) throw err;
 	list.forEach(function (file) {
 		if (patt.test(file))
 			console.log(file);
