0931c74b1b42681af0dced2380c2c704862d2b21
Colin Mackey <colin@fishbrains.com>
2013-12-21 15:11:09 -0500

    debug

--- solution_filter.js
+++ solution_filter.js
@@ -1,10 +1,15 @@
 var fs = require('fs');
 module.exports = function(dir, filterStr) {
 	var regex = new RegExp('\\.' + process.argv[3] + '$');
+
 	fs.readdir(dir, function(err, list) {
 		if (err) throw err;
 		list.forEach(function (file) {
-			return regex.test(file);
+			if (regex.test(file)) {
+				console.log("poopy");
+				return(file);
+				
+			}
 		});
-	}
+	});
 };
